6a52ec950fc7cabbed51e6f1c135f8265e63f483
Funtikov <Sergey.Funtikov@magenta-technology.com>
2024-12-13 14:09:48 +0400

    initial commit

---
+++ src/test/java/com/techstud/sch_auth/ScheduleAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techstud.sch_auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScheduleAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
