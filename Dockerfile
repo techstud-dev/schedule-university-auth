e9447a1e10b0438dabbb2979a986f056300557c1
EldarKabarxx <eldar200715@gmail.com>
2024-12-14 13:22:23 +0300

    SCHU-70: Dockerfile and swagger doc

--- Dockerfile
+++ Dockerfile
@@ -1,1 +1,19 @@
-## TODO: IMPLEMENT DOCKERFILE
+FROM maven:3.9.4-eclipse-temurin-17-alpine AS builder
+
+WORKDIR /build
+
+COPY pom.xml /build/
+RUN mvn dependency:go-offline
+
+COPY src /build/src
+RUN mvn clean package -DskipTests
+
+FROM openjdk:17-jdk-alpine
+
+WORKDIR /app
+
+COPY --from=builder /build/target/sch-auth-0.0.1-SNAPSHOT.jar /app/app.jar
+
+EXPOSE 8082
+
+CMD ["java", "-jar", "/app/app.jar"]
--- src/main/java/com/techstud/sch_auth/controller/AuthController.java
+++ src/main/java/com/techstud/sch_auth/controller/AuthController.java
@@ -7,15 +7,22 @@ import com.techstud.sch_auth.dto.SuccessAuthenticationDto;
 import com.techstud.sch_auth.service.AuthFacade;
 import com.techstud.sch_auth.util.CookieUtil;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.headers.Header;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.ErrorResponse;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequestMapping("/api/auth")
 public class AuthController {
@@ -39,7 +46,48 @@ public class AuthController {
     }
 
     @Operation(
-
+            summary = "Регистрация нового пользователя",
+            description = "Регистрирует нового пользователя в системе и возвращает токены доступа и обновления JWT." +
+                    " Файлы cookie для токенов устанавливаются в заголовках ответов.",
+            tags = {"Authentication"},
+            requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(
+                    description = "Данные регистрации пользователя",
+                    required = true,
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = RegisterDto.class)
+                    )
+            ),
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Пользователь успешно зарегистрировался",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = SuccessAuthenticationDto.class)
+                            ),
+                            headers = {
+                                    @Header(name = "Set-Cookie", description = "HttpOnly cookie содержит access token"),
+                                    @Header(name = "Set-Cookie", description = "HttpOnly cookie содержит refresh token")
+                            }
+                    ),
+                    @ApiResponse(
+                            responseCode = "400",
+                            description = "Ошибка валидации или неправильный запрос",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = ErrorResponse.class)
+                            )
+                    ),
+                    @ApiResponse(
+                            responseCode = "409",
+                            description = "Пользователь уже существует",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = ErrorResponse.class)
+                            )
+                    )
+            }
     )
     @PostMapping("/register")
     public ResponseEntity<?> register(@RequestBody RegisterDto registerDto) {
